3ad172d343eb985128a3e191ef1631f015b238c8
davidchanho <33166931+davidchanho@users.noreply.github.com>
2021-04-10 03:20:06 -0400

    add col

---
+++ components/_shared/col/index.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import Row from "../row";
+
+const Col = styled(Row)`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: column;
+`;
+
+export default Col;
